b0b78361da18b874d55e29e733c9ffb2872ea243
sintanbabo <sintanbabo@sudal-mac-mini.local>
2018-11-02 23:29:50 +0900

    Add Factory

---
+++ src/Factory/IDCardFactory.java
@@ -0,0 +1,23 @@
+package Factory;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class IDCardFactory extends Factory {
+	private List owners = new ArrayList();
+	
+	@Override
+	protected Product createProduct(String owner) {
+		return new IDCard(owner);
+	}
+
+	@Override
+	protected void registerProduct(Product product) {
+		owners.add(((IDCard)product).getOwner());
+	}
+	
+	public List getOwners() {
+		return owners;
+	}
+
+}
---
+++ src/Factory/Main.java
@@ -0,0 +1,15 @@
+package Factory;
+
+public class Main {
+	public static void main(String args[]) {
+		Factory factory = new IDCardFactory();
+		Product card1 = factory.create("A");
+		Product card2 = factory.create("B");
+		Product card3 = factory.create("C");
+		
+		card1.use();
+		card2.use();
+		card3.use();
+	}
+
+}
